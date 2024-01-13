3834d9524d749ddde63dcb9b714101a7f1c4140a
edetsan <detzsv@gmail.com>
2024-01-13 11:36:57 -0600

    grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage");
+        string percentage = Console.ReadLine();
+        int percentagenum = int.Parse(percentage);
+
+        string letter = "";
+
+        if (percentagenum >= 90)
+        {
+           letter = "A";
+        }
+        else if (percentagenum >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentagenum >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentagenum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}"); 
+
+        if (percentagenum >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass, keep trying!");
+        }
     }
 }
