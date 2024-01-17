b3c2a347b1e80725847ad5f2a879120f397309cf
edetsan <detzsv@gmail.com>
2024-01-16 22:19:29 -0600

    Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Nokia";
+        job1._jobTitle = "Network engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2014;
+
+        Job job2 = new Job();
+        job2._company = "Ericsson";
+        job2._jobTitle = "Technical Subject Matter Expert";
+        job2._startYear = 2014;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Detzani Santiago";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResumeDetails();
     }
 }
