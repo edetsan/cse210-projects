6be0baf23619a5860f0c828588fbcd2df6ec4ace
edetsan <detzsv@gmail.com>
2024-02-17 13:08:30 -0600

    Foundation 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video[] _videos = new Video[4];
+
+        _videos[0] = new Video("EmmaCruises", "7 Days In The Most Controversial BALCONY Cabin at Sea", 881);
+        _videos[0].AddComment("maryg4551", "Everything you described about this infinite verandah cabin was spot on!  We loved the Edge...the decor, the size, the food, Eden, the entertainment, etc.,  but I like having an ACTUAL balcony");
+        _videos[0].AddComment("carolfinnis6152", "An interesting concept but I think I prefer the standard balcony, thanks for showing us!");
+        _videos[0].AddComment("TheRadical42", "Hooray for Captain Hudson! What a cute mascot!");
+
+        _videos[1] = new Video("ShawnaEdwardsMusic", "RISEN - New Easter song by Shawna Edwards", 241);
+        _videos[1].AddComment("teresalittejohn1021", "Beautiful!!!!!  The REAL STORY of Jesus!  I love it.  You don't hear songs like these anymore, sadly.");
+        _videos[1].AddComment("ruthhalima", "Indeed Jesus died n was risen to set the captives free..... Nice melody and To God be the glory.");
+        _videos[1].AddComment("thecreativebudgeter", "There just are no words....");
+        
+        _videos[2] = new Video("KaraandNate", "We Slept in the World's Most Expensive Underwater Hotel", 1510);
+        _videos[2].AddComment("Supuntharindu168", "I was the survey engineer for making this masterpiece back in 2018. Really happy to see you there");
+        _videos[2].AddComment("jennclack", "As much as I’m happy that you’re taking care of yourself by taking time off, I’m so happy to see a new video! I’ve missed y’all! Thanks for another fun video.");
+        _videos[2].AddComment("aliciavandama2058", "Your video quality keeps getting better. Thank you for making your fans feel like family!");
+
+        _videos[3] = new Video("WildWomenExpeditions", "The Camino de Santiago Hiking Adventure", 61);
+        _videos[3].AddComment("shannononeil590", "This was such an amazing trip");
+        _videos[3].AddComment("LiveLifeLori", "This is on my list! Waiting for you to post a 2024 date :)");
+        _videos[3].AddComment("edetsan", "What a great adventure!!!");
+
+        for(int i = 0; i < _videos.Length ; i++)
+        {
+            Console.WriteLine();
+            _videos[i].DisplayVideoInfo();
+            _videos[i].DisplayVideoComments();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        //Console.Clear();
+        Address[] addresses =  new Address[2];
+        Customer[] customers =  new Customer[2];
+        List<Product>[] productsLists = new List<Product>[2];
+        Order[] orders = new Order[2];
+
+        //ORDER 1 (USA)
+        addresses[0] = new Address("7456 JENKINS AVE", "HESPERIA", "California", "USA");
+        customers[0] =  new Customer("Aaron Eustice",addresses[0]);
+        productsLists[0] = new List<Product>();
+        productsLists[0].Add(new Product("Portable Plug&Play Travel Laptop Monitor","T156",89.99f,2));
+        productsLists[0].Add(new Product("DEWALT 20V MAX Impact Driver, Cordless, 3-Speed, Battery and Charger Included","D845",169.00f,1));
+        productsLists[0].Add(new Product("Sony WH-1000XM4 Wireless Premium Noise Canceling Overhead Headphones","W104",348.00f,1));
+        orders[0] = new Order(customers[0],productsLists[0]);
+
+        //ORDER 2 (NOT USA)
+        addresses[1] = new Address("46 Manuel Payno", "Naucalpan de Juarez", "Estado de Mexico", "Mexico");
+        customers[1] =  new Customer("Detzani Santiago",addresses[1]);
+        productsLists[1] = new List<Product>();
+        productsLists[1].Add(new Product("Purple Hard Shell Luggage Set", "A333",149.99f,1));
+        productsLists[1].Add(new Product("Roborock S8 Pro Ultra","R0S8",1199.99f,1));
+        productsLists[1].Add(new Product("Leebein Electric Spin Scrubber, Cordless Powerful Scrub Brush","L246",39.99f,2));
+        productsLists[1].Add(new Product("HOUSE DAY Taupe Velvet Hangers 60 Pack","G630",19.99f,3));
+        orders[1] = new Order(customers[1],productsLists[1]);
+
+        for(int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"\n-----CUSTOMER N°{i+1} ORDER-----");
+            Console.WriteLine($"PACKING LABEL:\n{orders[i].GetPackingLabel()}"); 
+            Console.WriteLine($"SHIPPING LABEL\n{orders[i].GetShippingLabel()}");
+            Console.WriteLine($"TOTAL COST: ${orders[i].GetTotalCost()}");
+        }
     }
 }
